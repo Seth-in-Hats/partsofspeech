44fb89f9bbfb8b63d59d2f29965cd45bf41eccb8
Seth Lewis <lewiscatenary@gmail.com>
2019-02-03 11:41:19 +1300

    first commit. parts of speech prompts added. no narroration yet.

---
+++ randomstory.js
@@ -0,0 +1,5 @@
+var verb1 = prompt("Welcome, we're going to play a little game called Random Story. We're going to need a few parts of speech to make the story. Please provide a 'verb'", "Verb");
+var noun1 = prompt("Now please provide a 'noun'", "Noun");
+var nounProper = prompt("Next, we'll need a 'proper noun'");
+var verb2 = prompt("Aren't these pop up dialog boxes annoying? I'm sure there is a better way to do this. Next we'll be looking for another 'verb'", "Verb");
+var adjective = prompt("Last one, then we'll have a great random story for you to complete. 'Adjective' please", "Adjective");
