a62dca23e267eaa74aa37763f491396457445327
Seth Lewis <lewiscatenary@gmail.com>
2019-02-03 13:12:36 +1300

    added a name length calculator to challenge.js

---
+++ challenge.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var nameFirst = prompt("What is your first name?");
+var nameRap = prompt("What is your rap name?");
+var nameFirstRap = nameFirst + nameRap;
+var nameLength = nameFirstRap.length;
+
+document.write("Hello ", nameFirst, "also known as ", nameRap, ". The combined characters in both of your names is ",nameLength,".");
