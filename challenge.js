fce161786d50392966d3e36c9046d70030165734
Seth Lewis <lewiscatenary@gmail.com>
2019-02-03 13:14:19 +1300

    added a space after first name

--- challenge.js
+++ challenge.js
@@ -5,4 +5,4 @@ var nameRap = prompt("What is your rap name?");
 var nameFirstRap = nameFirst + nameRap;
 var nameLength = nameFirstRap.length;
 
-document.write("Hello ", nameFirst, "also known as ", nameRap, ". The combined characters in both of your names is ",nameLength,".");
+document.write("Hello ", nameFirst, " also known as ", nameRap, ". The combined characters in both of your names is ",nameLength,".");
